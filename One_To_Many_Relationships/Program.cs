5a5c986e67d8c406555275739c2ee70cf4780089
Mustafa <mustafa.barlas1146@gmail.com>
2024-05-07 21:37:09 +0300

    files added

---
+++ One_To_Many_Relationships/Program.cs
@@ -0,0 +1,121 @@
+﻿// ReSharper disable all
+
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+
+
+#region Default Convention
+
+//public class Employee
+//{
+//    public int Id { get; set; }
+//    public string? Name { get; set; }
+
+//    public int? DepartmentId { get; set; } // Zorunlu değil
+//    public Department? Department { get; set; }
+//}
+
+//public class Department
+//{
+//    public int Id { get; set; }
+//    public string? Name { get; set; }
+
+//    public ICollection<Department>? Departments { get; set; }
+//}
+
+#endregion
+
+#region Data Annotations
+
+//public class Employee
+//{
+//    public int Id { get; set; }
+//    public string? Name { get; set; }
+
+//    [ForeignKey(nameof(Department))]
+//    public int? DepartmentId { get; set; } // Zorunlu 
+//    public Department? Department { get; set; }
+//}
+
+//public class Department
+//{
+//    public int Id { get; set; }
+//    public string? Name { get; set; }
+
+//    public ICollection<Department>? Departments { get; set; }
+//}
+
+#endregion
+
+#region Fluent Api
+
+//public class Employee
+//{
+//    public int Id { get; set; }
+//    public string? Name { get; set; }
+
+//    public int? DepartmentId { get; set; }  // Zorunlu değil
+//    public Department? Department { get; set; }
+//}
+
+//public class Department
+//{
+//    public int Id { get; set; }
+//    public string? Name { get; set; }
+
+//    public ICollection<Employee>? Employees { get; set; }
+//}
+
+
+//public class ETradeDbContext : DbContext
+//{
+
+//    public DbSet<Department> Departments { get; set; }
+//    public DbSet<Employee> Employees { get; set; }
+
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//    {
+//        optionsBuilder.UseSqlServer("Server = MUSTAFABRLS; Database = ETRADE; Trusted_Connection = true; TrustServerCertificate = True;");
+//    }
+
+
+//    protected override void OnModelCreating(ModelBuilder modelBuilder)
+//    {
+//        modelBuilder.Entity<Employee>()
+//            .HasOne(x => x.Department)
+//            .WithMany(x => x.Employees)
+//            .HasForeignKey(x => x.DepartmentId); // Zorunlu değil
+//    }
+//}
+
+#endregion
+
+
+
+public class ETradeDbContext : DbContext
+{
+
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server = MUSTAFABRLS; Database = ETRADE; Trusted_Connection = true; TrustServerCertificate = True;");
+    }
+}
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
+
+public class Department
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+
+}
---
+++ One_To_OneRelationships/Program.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+// ReSharper disable all
+//Principal  kendi başına var olabilen entitydir.
+//Dependent  kendi başına var olamayan entitydir.
+
+
+
+#region Default Convention
+
+//public class Employee
+//{
+//    public int Id { get; set; }
+//    public string? Name { get; set; }
+
+//    public Address? Address { get; set; }
+
+//}
+
+//public class Address
+//{
+//    public int Id { get; set; }
+//    public int? EmployeeId { get; set; }
+//    public string? AddressText { get; set; }
+
+//    public Employee? Employee { get; set; }
+//}
+
+#endregion
+
+#region Data Annotations
+
+//public class Employee
+//{
+//    public int Id { get; set; }
+//    public string? Name { get; set; }
+
+//    public Address? Address { get; set; }
+
+//}
+
+//public class Address
+//{
+//    public int Id { get; set; }
+
+//    [ForeignKey(nameof(Employee))]
+//    public int? EmployeeId { get; set; }
+//    public string? AddressText { get; set; }
+
+//    public Employee? Employee { get; set; }
+//}
+
+// ***********************   OR
+//public class Address
+//{
+//    [Key,ForeignKey(nameof(Employee))]
+//    public int Id { get; set; }
+
+//    public string? AddressText { get; set; }
+
+//    public Employee? Employee { get; set; }
+//}
+
+#endregion
+
+#region Fluent Api
+
+//public class ETradeDbContext : DbContext
+//{
+
+//    public DbSet<Address> Addresses { get; set; }
+//    public DbSet<Employee> Employees { get; set; }
+
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//    {
+//        optionsBuilder.UseSqlServer("Server = MUSTAFABRLS; Database = ETRADE; Trusted_Connection = true; TrustServerCertificate = True;");
+//    }
+
+//    protected override void OnModelCreating(ModelBuilder modelBuilder)
+//    {
+//        modelBuilder.Entity<Address>().HasKey(x => x.Id);
+
+//        modelBuilder.Entity<Employee>()
+//            .HasOne(x => x.Address)
+//            .WithOne(x => x.Employee)
+//            .HasForeignKey<Address>(x => x.Id);
+//    }
+//}
+#endregion
+
+
+
+public class ETradeDbContext : DbContext
+{
+
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server = MUSTAFABRLS; Database = ETRADE; Trusted_Connection = true; TrustServerCertificate = True;");
+    }
+}
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+
+    public Address? Address { get; set; }
+
+}
+
+public class Address
+{
+    public int Id { get; set; }
+
+    public string? AddressText { get; set; }
+
+    public Employee? Employee { get; set; }
+}
