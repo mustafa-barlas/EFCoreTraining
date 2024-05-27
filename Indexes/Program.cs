85b23eea811eaf449cbfc16c2c5aacfbf5f03768
Mustafa <mustafa.barlas1146@gmail.com>
2024-05-27 22:08:35 +0300

    files added

---
+++ Indexes/Program.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore;
+ApplicationContext context = new ApplicationContext();
+//resharper disable all
+
+
+#region Index Attribute
+
+[Index(nameof(Name))]
+public class Person1
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+
+
+
+#endregion
+
+#region Index Fluent Api
+
+public class ApplicationContext1 : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)  // 
+    {
+        modelBuilder.Entity<Person1>().HasIndex(x => x.Name);
+    }
+
+}
+
+#endregion
+
+#region Composite key
+
+[Index(nameof(Name), nameof(Surname))]
+public class Person2
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+
+
+
+public class ApplicationContext2 : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)  // 
+    {
+        modelBuilder.Entity<Person1>().HasIndex(x => new { x.Name, x.Surname });
+        modelBuilder.Entity<Person1>().HasIndex(nameof(Person.Name), nameof(Person.Surname));
+    }
+
+}
+
+#endregion
+
+#region Index Uniqueness
+
+[Index(nameof(Name), IsUnique = true)]
+public class Person3
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+
+
+public class ApplicationContext3 : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)  // 
+    {
+        modelBuilder.Entity<Person3>().HasIndex(nameof(Person.Name)).IsUnique();
+    }
+
+}
+
+#endregion
+
+#region Index Sort Order
+
+[Index(nameof(Name), nameof(Surname), AllDescending = true)]                  // tüm indexlerde uygulanır.
+[Index(nameof(Name), nameof(Surname), IsDescending = new[] { true, false })] //  ilki sıralanacak ikincisi sıralanmaycak.
+public class Person4
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+
+
+public class ApplicationContext4 : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)  // 
+    {
+        modelBuilder.Entity<Person>()
+            .HasIndex(x => new { x.Name, x.Surname })
+            .IsDescending(true, false); //  ilki sıralanacak ikincisi sıralanmaycak.
+
+
+    }
+
+}
+
+#endregion
+
+
+
+public class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)  // 
+    {
+
+    }
+
+}
---
+++ Sequences/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml.Schema;
+using Microsoft.EntityFrameworkCore;
+//resharper disable all
+namespace Sequences;
+
+
+#region Sequence tanımlama 
+
+// benzersiz ve ardışık sayısal değerler üreten veritabanı nesnesidir. tablo özeliği deildir. birden fazla tablo tarafından kullanılabilir.
+
+public class ApplicationContext1 : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)  // 
+    {
+        
+    }
+
+}
+
+#endregion
+
+
+
+
+
+public class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+}
+
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)  // 
+    {
+
+    }
+
+}
